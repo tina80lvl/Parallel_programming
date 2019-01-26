84b68bea1eb64194fd6ce60dce88523d8cf1d7b6
Tina Smirnova <tinanight98@gmail.com>
2019-01-26 16:41:11 +0300

    Padding added

--- sharded-counter-tina80lvl/src/main/java/counter/CounterImpl.java
+++ sharded-counter-tina80lvl/src/main/java/counter/CounterImpl.java
@@ -2,29 +2,24 @@ package counter;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class CounterImpl implements Counter {
     private static int AVIL_SLOTS = 2 * Runtime.getRuntime().availableProcessors();
+    private int PADDING = 16;
     private Random random = ThreadLocalRandom.current();
-    private AtomicInteger[] slots = new AtomicInteger[AVIL_SLOTS];
-
-    CounterImpl() {
-        for (int i = 0; i < AVIL_SLOTS; i++) {
-            slots[i] = new AtomicInteger();
-        }
-    }
+    private AtomicIntegerArray slots = new AtomicIntegerArray(AVIL_SLOTS * PADDING);
 
     @Override
     public void inc() {
-        slots[random.nextInt(AVIL_SLOTS)].getAndIncrement();
+        slots.getAndAdd(random.nextInt(AVIL_SLOTS) * PADDING, 1);
     }
 
     @Override
     public int get() {
         int res = 0;
-        for (AtomicInteger slot : slots) {
-            res += slot.get();
+        for (int i = 0; i < AVIL_SLOTS; ++i) {
+            res += slots.get(i * PADDING);
         }
         return res;
     }
